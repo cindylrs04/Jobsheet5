f74e5e28c074cc000d4ce7db7a8f3fede0895ce1
cindylrs04 <larasaticindy231@gmail.com>
2023-09-25 12:47:00 +0700

    ternary

--- PemilihanPercobaan108.java
+++ PemilihanPercobaan108.java
@@ -5,12 +5,14 @@ public class PemilihanPercobaan108 {
     System.out.print("Masukkan angka: ");
     int angka = input00.nextInt();
     
-    if (angka % 2 == 0) {
-        System.out.println("Angka "+angka+" bilangan genap");
-    } else {
-        System.out.println("Angka "+angka+"bilangan ganjil");
+    // if (angka % 2 == 0) {
+    //     System.out.println("Angka "+angka+" bilangan genap");
+    // } else {
+    //     System.out.println("Angka "+angka+"bilangan ganjil");
         
 
-    }
+    // }
+    String hasil = (angka % 2 == 0) ? "Angka " + angka + " bilangan genap" : "Angka " + angka + " bilangan ganjil";
+    System.out.println(hasil);
 }
 }
